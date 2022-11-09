64a0b20db651dbd15679234c189b9434d399677c
unknown <gabor.grunhut@mail.hu>
2022-11-09 09:07:26 +0100

    Kezdés

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'gyepes';
+  diameter!:number;
+  price!:number;
+
+  calcArea():void{
+    let radius = this.diameter/2;
+    let area = Math.pow(radius, 2* Math.PI);
+    let halfArea = area/2;
+    this.price = halfArea * 2500;
+    console.log('Működik')
+  }
 }
